339eac90306e3908782d3b931bb5abeccc1b6f33
420NA <120449314+420NA@users.noreply.github.com>
2023-02-20 08:16:42 +0000

    a

--- Zadaci_Sa_Komntrolnog/zadatak2.c
+++ Zadaci_Sa_Komntrolnog/zadatak2.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
+int sum(int arr[], int n){
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+       sum+=arr[i];
+    }
+    return sum;
+}
 int main (int argc, char *argv[]) {
     int n;
     scanf("%d", &n);
     int arr[n];
     for (int i = 0; i < n; i++)
     {
-        /* code */
+       scanf("%d", &arr[i]);
     }
-    
+    printf("%d", sum(arr,n));
     return 0;
 }
